30adf5bffc6f9e748157b509e402dcf36cf31d41
Divit <onlyego1043@gmail.com>
2026-09-20 14:10:26 +0530

    fixed

--- app/interns/[slug]/page.tsx
+++ app/interns/[slug]/page.tsx
@@ -29,8 +29,9 @@ export function generateStaticParams() {
   }));
 }
 
-export default function InternPage({ params }: { params: { slug: string } }) {
-  const intern = INTERNS.find((i) => i.slug === params.slug);
+export default async function InternPage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
+  const intern = INTERNS.find((i) => i.slug === slug);
 
   if (!intern) {
     notFound();
